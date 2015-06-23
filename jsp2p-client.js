ccd063942e2ade0b8101427b95e85156ee1f9f1d
Pierre ROTH <pierre.roth@legrand.fr>
2015-06-23 16:31:29 +0200

    Connection information is now encapsulated

--- jsp2p-client.js
+++ jsp2p-client.js
@@ -1,8 +1,22 @@
+/* Copyright (C) 2015 Legrand France
+ * All rights reserved
+ *
+ * JS library to deal with Legrand P2P servers (XMPP)
+ */
 'use strict';
 
 var nxmlpp = require('nxmlpp');
 var xmpp = require('simple-xmpp');
 var argv = process.argv;
+
+function ConnectioInfo() {
+    this.jid = argv[2];
+    this.pwd = argv[3];
+    this.host = argv[4];
+    this.port = 5222;
+}
+
+var connInfo = new ConnectioInfo();
 
 if (argv.length < 3) {
     console.error('Usage: node jsp2p-client.js <myjid> ' +
@@ -27,19 +41,30 @@ xmpp.on('error', function(err) {
 });
 
 xmpp.on('stanza', function(stanza) {
-     console.log('Incoming stanza:\n', nxmlpp.strPrint(stanza.toString()));
+    console.log('Incoming stanza:', nxmlpp.strPrint(stanza.toString()));
+    if (stanza.is('iq') && stanza.attrs.type === 'result' && stanza.attrs.id === 'roster_0') {
+        var buddies = stanza.children[0].children;
+        console.log("buddies:", buddies);
+    }
 });
 
-xmpp.on('subscribe', function(from) {
-// if (from === 'a.friend@gmail.com') {
-//     xmpp.acceptSubscription(from);
-//     }
+process.on('SIGINT', function() {
+    console.log("\nCaught ctrl+c");
+    console.log("Disconnecting...");
+    xmpp.disconnect();
+    console.log("Exiting...");
+    process.exit();
 });
 
 xmpp.connect({
-        jid                 : argv[2],
-        password            : argv[3],
-        host                : argv[4],
-        port                : 5222
+        jid                 : connInfo.jid,
+        password            : connInfo.pwd,
+        host                : connInfo.host,
+        port                : connInfo.port
 });
 
+console.log('Sending presence...');
+xmpp.setPresence('away', 'Swimming :-D \o/');
+
+console.log('Asking for roster...');
+xmpp.getRoster();
