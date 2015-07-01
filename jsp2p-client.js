a40abc0dcbd95e9528c8ad0648c206f5515a0bac
Pierre ROTH <pierre.roth@legrand.fr>
2015-07-01 13:57:17 +0200

    Rework client based on node-xmpp-client module

--- jsp2p-client.js
+++ jsp2p-client.js
@@ -13,76 +13,78 @@ var STATUS = {
     OFFLINE: "offline"
 };
 
-var nxmlpp = require('nxmlpp');
-var xmpp = require('simple-xmpp');
+var Client = require('node-xmpp-client'),
+    ltx = require('node-xmpp-core').ltx,
+    nxmlpp = require('nxmlpp'),
+    log4js = require('log4js'),
+    program = require('commander');
 
-var log4js = require('log4js');
 log4js.configure({
   appenders: [
     { type: 'console' },
   ]
 });
 
-var logger = log4js.getLogger('jsp2p');
-
-var program = require('commander');
 program
   .version('0.1.0')
   .usage('[options] <JID> <password> <host>')
   .option('-d --debug <level>', 'Log level', /^(DEBUG|INFO|ERROR)$/i, 'INFO')
   .option('-port, --port <port>', 'XMPP port to connect to', parseInt)
+  .option('-r, --register', 'Ask for XMPP registration before any further operation')
   .parse(process.argv);
 
 if (process.argv.slice(2).length < 3) {
-    program.help();
+  program.help();
 }
 
-logger.setLevel(program.debug);
+var options = {
+  jid: process.argv[2],
+  password: process.argv[3],
+  host: process.argv[4],
+  port: program.port || 5222,
+  register: program.register || false,
+};
 
-function ConnectioInfo() {
-    this.jid = process.argv[2];
-    this.pwd = process.argv[3];
-    this.host = process.argv[4];
-    this.port = program.port || 5222;
+var util = require('util');
+
+function JsP2PClient(options) {
+  this.logger = log4js.getLogger('jsp2p');
+  this.logger.setLevel(program.debug);
+  this.logger.debug('JsP2PClient creation options:', options);
+  Client.call(this, options);
 }
 
-var connInfo = new ConnectioInfo();
+util.inherits(JsP2PClient, Client);
 
-xmpp.on('online', function(data) {
-    logger.info('Connected with JID:', data.jid.user);
+
+JsP2PClient.prototype.getRoster = function () {
+  var roster = new ltx.Element('iq',{id: 'roster_0',type: 'get'}).c('query', { xmlns: 'jabber:iq:roster'});
+  this.logger.debug('Send get roster request:', roster);
+  this.send(roster);
+};
+
+var client = new JsP2PClient(options);
+
+client.addListener('online', function(data) {
+  client.send('<presence/>');
+  this.logger.info('Connected with JID:', data.jid.user);
 });
 
-xmpp.on('error', function(err) {
-    logger.error(err);
+client.addListener('stanza', function(stanza) {
+  this.logger.debug('Incoming stanza:', nxmlpp.strPrint(stanza.toString()));
+  if (stanza.is('iq') && stanza.attrs.type === 'result' && stanza.attrs.id === 'roster_0') {
+      var buddies = stanza.children[0].children;
+      this.logger.debug("buddies:", buddies);
+  }
 });
 
-xmpp.on('stanza', function(stanza) {
-    logger.debug('Incoming stanza:', nxmlpp.strPrint(stanza.toString()));
-    if (stanza.is('iq') && stanza.attrs.type === 'result' && stanza.attrs.id === 'roster_0') {
-        var buddies = stanza.children[0].children;
-        logger.debug("buddies:", buddies);
-    }
+client.addListener('connect', function () {
+    this.logger.debug('Client is connected');
 });
 
-process.on('SIGINT', function() {
-    logger.debug("\nCaught ctrl+c");
-    logger.debug("Disconnecting...");
-    xmpp.disconnect();
-    logger.info("Exiting...");
-    process.exit();
+client.addListener('error', function(e) {
+    this.logger.error(e);
 });
 
-logger.debug('Connecting to %s:%d with %s', connInfo.host, connInfo.port, connInfo.jid);
 
-xmpp.connect({
-        jid                 : connInfo.jid,
-        password            : connInfo.pwd,
-        host                : connInfo.host,
-        port                : connInfo.port
-});
-
-logger.debug('Sending presence...');
-xmpp.setPresence(STATUS.ONLINE, 'At work!');
-
-logger.debug('Asking for roster...');
-xmpp.getRoster();
+client.getRoster();
